2eb0412b02f003f971cf90883486a23b57a8b6f5
raymondkneipp <raymond.f.kneipp@gmail.com>
2023-05-09 10:37:27 -0700

    finished first two presence validations

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,3 +1,5 @@
 class Apartment < ApplicationRecord
     belongs_to :user
+
+    validates :street, :unit, presence: true
 end
--- spec/models/apartment_spec.rb
+++ spec/models/apartment_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Apartment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create(email: 'testing@example.com', password: 'password', password_confirmation: 'password') }
+
+  it('should validate street') do
+    apartment = user.apartments.create(
+      unit: '200',
+      city: 'cityName',
+      state: 'stateName',
+      square_footage: 1400,
+      price: '1600',
+      bedrooms: 3,
+      bathrooms: 1.5,
+      pets: 'yes!',
+      image: 'image.url'
+    )
+
+    expect(apartment.errors[:street]).to include "can't be blank"
+  end
+
+  # unit test
+  it('should validate unit') do
+    apartment = user.apartments.create(
+      street: '5 main street',
+      city: 'cityName',
+      state: 'stateName',
+      square_footage: 1400,
+      price: '1600',
+      bedrooms: 3,
+      bathrooms: 1.5,
+      pets: 'yes!',
+      image: 'image.url'
+    )
+
+    expect(apartment.errors[:unit]).to include "can't be blank"
+  end
+
+  # city test - ray
+  # state test - ray
+  # square_footage test - dre
+  # price test - dre
+  # bedrooms test - dre
+  # bathrooms test - nguyen
+  # pets test - nguyen
+  # image test - nguyen
 end
