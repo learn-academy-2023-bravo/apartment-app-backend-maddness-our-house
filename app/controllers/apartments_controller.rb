45ba1dfa32a429d6a85e0a40a1913b8aec2aeaa7
D'Andre Moore <126428981+dmooredidit@users.noreply.github.com>
2023-05-09 14:56:46 -0700

    completed read functionality

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -3,4 +3,36 @@ class ApartmentsController < ApplicationController
         apartments = Apartment.all
         render json: apartments
     end
+
+    def create
+        apartment = Apartment.create(apartment_params)
+        if apartment.valid?
+            render json: apartment
+        else
+            render json: apartments.errors
+        end
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+    def apartment_params
+    params.require(:apartment).permit(
+        :user_id,
+        :street,
+        :unit,
+        :city,
+        :state,
+        :square_footage,
+        :price,
+        :bedrooms,
+        :bathrooms,
+        :pets,
+        :image,
+        :manager)
+    end
 end
