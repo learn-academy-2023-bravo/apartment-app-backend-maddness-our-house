42cf59141b1aaf3010a70405994df959296a478a
D'Andre Moore <126428981+dmooredidit@users.noreply.github.com>
2023-05-09 11:24:57 -0700

    last of the test

--- spec/models/apartment_spec.rb
+++ spec/models/apartment_spec.rb
@@ -68,6 +68,54 @@ RSpec.describe Apartment, type: :model do
 
     expect(apartment.errors[:state]).to include "can't be blank"
   end
+  # square_footage test - Dre
+  it('should validate square footage') do
+    apartment = user.apartments.create(
+      street: '5 main street',
+      unit: '200',
+      city: 'cityName',
+      state: 'stateName',
+      price: '1600',
+      bedrooms: 3,
+      bathrooms: 1.5,
+      pets: 'yes!',
+      image: 'image.url'
+    )
+
+    expect(apartment.errors[:square_footage]).to include "can't be blank"
+  end
+# price test - Dre
+  it('should validate price') do
+    apartment = user.apartments.create(
+      street: '5 main street',
+      unit: '200',
+      city: 'cityName',
+      state: 'stateName',
+      square_footage: 1400,
+      bedrooms: 3,
+      bathrooms: 1.5,
+      pets: 'yes!',
+      image: 'image.url'
+    )
+
+    expect(apartment.errors[:price]).to include "can't be blank"
+  end
+# bedrooms test - Dre
+  it('should validate bedrooms') do
+    apartment = user.apartments.create(
+      street: '5 main street',
+      unit: '200',
+      city: 'cityName',
+      state: 'stateName',
+      square_footage: 1400,
+      price: '1600',
+      bathrooms: 1.5,
+      pets: 'yes!',
+      image: 'image.url'
+    )
+
+    expect(apartment.errors[:bedrooms]).to include "can't be blank"
+  end
   # square_footage test - dre
   # price test - dre
   # bedrooms test - dre
